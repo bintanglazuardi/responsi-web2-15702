8bb2ba20ef17916ab1a5037a4059cf9226f1d5d9
Bintang Lazuardi <bintanglazuardi@mail.ugm.ac.id>
2019-12-17 14:57:59 +0700

    revisi2

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -5,7 +5,7 @@
   @csrf
   <div class="form-group">
     <label>Nama Jobs</label>
-    <input type="text" name="nama" class="form-control" placeholder="...">
+    <input type="text" name="name" class="form-control" placeholder="...">
   </div>
   <button type="submit" class="btn btn-primary">Simpan</button>
 </form>
